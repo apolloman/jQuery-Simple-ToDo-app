5d2ba18a3cd5255ed24528244fd6cc921aa11695
Roma Mus <apollo5577@gmail.com>
2017-03-02 15:55:36 +0700

    add js folder

---
+++ js/jscript.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  $("#add").on('click', function() {
+    var val = $("input").val();
+    if(val !== '') {
+        var elem = $("<li></li>").text(val);
+      $(elem).append("<button class='remove'>X</button>");
+      $("#myList").append(elem);
+      $("input").val("");
+      $(".remove").on('click', function(){
+        $(this).parent().remove();
+      });
+    }
+
+    $("#myList > li").on('click', function(){
+      $(this).toggleClass('line');
+    });
+
+  });
+
+
+
+
+
+}); // END Ready function
+
+
+(function(){
+  var icon = document.querySelector('.icon');
+  icon.addEventListener('click', changeIcon);
+  var todoCon = document.querySelector('.todoCon');
+
+  function changeIcon() {
+      icon.classList.toggle('change');
+      todoCon.classList.toggle('display');
+  }
+
+})();
+
+(function displayTime(){
+   var elem = document.getElementById('clock');
+   var time = new Date();
+   elem.innerHTML = time.toLocaleTimeString();
+   setTimeout(displayTime, 1000);
+})();
